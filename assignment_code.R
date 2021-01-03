883f6ad19f02d5e0c3b2086490c48ad0e61d9eba
liuluyang1104 <liuluyang1104@gmail.com>
2021-01-03 21:52:10 +0000

    commit

--- assignment_code.R
+++ assignment_code.R
@@ -10,7 +10,19 @@ library(sf)
 library(geojson)
 library(geojsonio)
 library(tmaptools)
+library(dplyr)
+library(readr)
 
 # loading data
 LondonWards <- st_read('C:/Users/Louriee/Desktop/gis/assignment/Assignment/
                        London-wards-2018/London-wards-2018_ESRI/London_Ward.shp')
+LondonWardsMerged <- st_read("C:/Users/Louriee/Desktop/gis/assignment/Assignment/
+                             London-wards-2018/London-wards-2018_ESRI/
+                             London_Ward_CityMerged.shp")%>%
+  st_transform(.,27700)
+library(readr)
+obesity <- read_csv("obesity.csv")
+LondonWardsMerged <- LondonWardsMerged %>%
+  left_join(WardData,
+            by = c("GSS_CODE" = "new_code"))%>%
+  distinct(GSS_CODE, ward_name, average_gcse_capped_point_scores_2014)
